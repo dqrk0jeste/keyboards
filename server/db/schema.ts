5bdd65f72a4e8e4f1a3d637add90008eca70fba7
dqrk0jeste <darkonikoloc@gmail.com>
2024-05-31 01:06:12 +0200

    some more work on tables

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, uuid, integer, varchar, text, index } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, integer, varchar, text, index, boolean, pgEnum } from 'drizzle-orm/pg-core'
 
 export const keyboards = pgTable('keyboards', {
   id: uuid('id').primaryKey().defaultRandom(),
@@ -10,9 +10,76 @@ export const keyboards = pgTable('keyboards', {
 }, (table) => {
     return {
       nameIndex: index('name_index').on(table.name),
+      priceIndex: index('price_index').on(table.price),
     }
   }
 );
 
-export type User = typeof keyboards.$inferSelect; 
-export type NewUser = typeof keyboards.$inferInsert;
+export type Keyboard = typeof keyboards.$inferSelect 
+export type NewKeyboard = typeof keyboards.$inferInsert
+
+export const switches = pgTable('switches', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  name: varchar('name', {
+    length: 255,
+  }).notNull(),
+  price: integer('price').notNull(),
+  description: text('description'),
+}, (table) => {
+  return {
+    priceIndex: index('price_index').on(table.price),
+  }
+})
+
+export type Switch = typeof switches.$inferSelect
+export type NewSwitch = typeof switches.$inferInsert
+
+export const keycaps = pgTable('keycaps', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  name: varchar('name', {
+    length: 255,
+  }).notNull(),
+  price: integer('price').notNull(),
+  description: text('description'),
+}, (table) => {
+  return {
+    priceIndex: index('price_index').on(table.price),
+  }
+})
+
+export type Keycaps = typeof keycaps.$inferSelect
+export type NewKeycaps = typeof keycaps.$inferInsert
+
+export const keycapFormatOptions = pgEnum('keycap_options', [
+  'windows',
+  'mac',
+])
+
+export const orders = pgTable('orders', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  keyboardId: uuid('keyboard_id').references(() => keyboards.id).notNull(),
+  switchId: uuid('switch_id').references(() => switches.id).notNull(),
+  keycapId: uuid('keycap_id').references(() => keycaps.id).notNull(),
+  handlubedSwitches: boolean('handlubed_switches').notNull().default(false),
+  keycapFormat: keycapFormatOptions('keycap_options').notNull().default('windows'),
+  extraFoam: boolean('extra_foam').notNull().default(false),
+  customerName: varchar('customer_name', {
+    length: 255,
+  }).notNull(),
+  address: varchar('address', {
+    length: 255,
+  }).notNull(),
+  phoneNumber: varchar('address', {
+    length: 255,
+  }).notNull(),
+  note: varchar('note', {
+    length: 255,
+  }).notNull(),
+}, (table) => {
+  return {
+      
+    }
+}) 
+
+export type Order = typeof orders.$inferSelect
+export type NewOrder = typeof orders.$inferInsert
