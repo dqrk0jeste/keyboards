ae79bc1fb341cc1e5485da8c4b7b6e4913886199
dqrk0jeste <darkonikoloc@gmail.com>
2024-06-18 17:41:21 +0200

    form done, will test it tmrw

--- server/api/form.post.ts
+++ server/api/form.post.ts
@@ -39,73 +39,158 @@ const bodySchema = z.object({
   maxPrice: z.number().gt(5000).finite(),
   format: formats,
   pudding: z.boolean(),
-  colors: colors.array().nonempty().max(2),
+  mainColor: colors,
+  otherColor: colors.nullish(),
   switchType: switchTypes,
   bluetooth: z.boolean(),
   wireless: z.boolean(),
 })
 
+type Filter<T> = {
+  matching: T[],
+  other: T[], 
+} 
+
 type KeyboardWithColor = {
   keyboards: Keyboard,
   keyboard_colors: KeyboardColor,
 }
-type FilterKeyboardsReturn = {
-  matching: {
-    keyboards: Keyboard,
-    keyboard_colors: KeyboardColor,
-  }[],
-  other: {
-    keyboards: Keyboard,
-    keyboard_colors: KeyboardColor,
-  }[], 
-} 
 
 type FilterKeyboardsOptions = {
   maxPrice: number,
   format: Format,
   bluetooth: boolean,
   wireless: boolean,
-  colors: Color[],
+  mainColor: Color,
+  otherColor: Color | undefined | null,
 }
 
-function filterKeyboards(keyboards: KeyboardWithColor[], options: FilterKeyboardsOptions): FilterKeyboardsReturn {
-  const matching = keyboards.filter(keyboard => {
-      return keyboard.keyboards.price <= options.maxPrice - 5000
+function filterKeyboards(keyboards: KeyboardWithColor[], options: FilterKeyboardsOptions): Filter<KeyboardWithColor> {
+  const matchingMainColor = [] as KeyboardWithColor[]
+  const matchingSecondaryColor = [] as KeyboardWithColor[]
+  const other = [] as KeyboardWithColor[]
+  for(const keyboard of keyboards) {
+    if(
+      keyboard.keyboards.price <= options.maxPrice - 5000
       && keyboard.keyboards.format === options.format
       && (!options.wireless || keyboard.keyboards.isWireless)
       && (!options.bluetooth || keyboard.keyboards.isBluetooth)
-      && options.colors.includes(keyboard.keyboard_colors.color as Color)
-    })
+    ) {
+      if(options.mainColor === keyboard.keyboard_colors.color) {
+        matchingMainColor.push(keyboard)
+      } else if(options.otherColor === keyboard.keyboard_colors.color) {
+        matchingSecondaryColor.push(keyboard)
+      }
+    } else {
+      other.push(keyboard)
+    }
+  }
+
+  return {
+    matching: [...matchingMainColor, ...matchingSecondaryColor],
+    other,
+  }
 }
 
-function filterPrebuiltKeyboards(keyboards: KeyboardWithColor[], options: FilterKeyboardsOptions): FilterKeyboardsReturn {
-  return keyboards.filter(keyboard => {
-    return keyboard.keyboards.isPrebuilt
+function filterPrebuiltKeyboards(keyboards: KeyboardWithColor[], options: FilterKeyboardsOptions): Filter<KeyboardWithColor> {
+  const matchingMainColor = [] as KeyboardWithColor[]
+  const matchingSecondaryColor = [] as KeyboardWithColor[]
+  const other = [] as KeyboardWithColor[]
+  for(const keyboard of keyboards) {
+    if(
+      keyboard.keyboards.isPrebuilt
       && keyboard.keyboards.price <= options.maxPrice
       && keyboard.keyboards.format === options.format
       && (!options.wireless || keyboard.keyboards.isWireless)
       && (!options.bluetooth || keyboard.keyboards.isBluetooth)
-      && options.colors.includes(keyboard.keyboard_colors.color as Color)
-  }) 
+    ) {
+      if(options.mainColor === keyboard.keyboard_colors.color) {
+        matchingMainColor.push(keyboard)
+      } else if(options.otherColor === keyboard.keyboard_colors.color) {
+        matchingSecondaryColor.push(keyboard)
+      }
+    } else {
+      other.push(keyboard)
+    }
+  }
+
+  return {
+    matching: [...matchingMainColor, ...matchingSecondaryColor],
+    other,
+  }
 }
 
-function filterSwitches(switches: Switch[], switchType: SwitchType): Switch[] {
-   return switches.filter(s => s.type === switchType)
+function filterSwitches(switches: Switch[], switchType: SwitchType): Filter<Switch> {
+  const matching = [] as Switch[]
+  const other = [] as Switch[]
+  for(const s of switches) {
+    if(s.type === switchType) {
+      matching.push(s)
+    } else {
+      other.push(s)
+    }
+  }
+  return {
+    matching,
+    other,
+  }
 }
 
-function filterKeycaps(keycaps: Keycap[], options: {
-  colors: Color[],
+type FilterKeycapsOptions = {
+  mainColor: Color,
+  otherColor: Color | undefined | null,
   pudding: boolean,
-}): Keycap[] {
-  return keycaps.filter(keycap => {
-    const keycapColors = keycap.accentColors?.split(' ')
-    const hasRightColorsInAccent = keycapColors?.reduce((current, color) => {
-      return current || options.colors.includes(color as Color)
-    }, false)
-    return (options.colors.includes(keycap.mainColor as Color)
-      || hasRightColorsInAccent)
-      && (!options.pudding || keycap.isPudding)
-  })
+}
+
+type Ranking<T> = {
+  value: T,
+  rank: number,
+}
+
+function filterKeycaps(keycaps: Keycap[], options: FilterKeycapsOptions): Filter<Keycap> {
+  const matching = [] as Ranking<Keycap>[]
+  const other = [] as Keycap[]
+
+  for(const keycap of keycaps) {
+    const entry = {
+      value: keycap,
+      rank: 0,
+    }
+
+    if(keycap.mainColor === options.mainColor) {
+      entry.rank += 3
+    } else if(keycap.mainColor === options.otherColor) {
+      entry.rank += 1
+    }
+
+    if(keycap.accentColors) {
+      const accentColors = keycap.accentColors.split(' ') as Color[]
+      if(accentColors.includes(options.mainColor)) {
+        entry.rank += 1
+      }
+      if(options.otherColor && accentColors.includes(options.otherColor)) {
+        entry.rank += 1
+      }
+    }
+
+    if(entry.rank === 0) {
+      other.push(keycap)
+    } else if(!options.pudding || keycap.isPudding){
+      matching.push(entry)
+    } else {
+      other.push(keycap)
+    }
+  }
+
+  return {
+    matching: matching.sort((a, b) => {
+      if(a.rank > b.rank) {
+        return 1
+      }
+      return -1
+    }).map(r => r.value),
+    other,
+  }
 }
 
 export default defineEventHandler(async (e) => {
@@ -120,7 +205,8 @@ export default defineEventHandler(async (e) => {
   const {
     maxPrice,
     format,
-    colors,
+    mainColor,
+    otherColor,
     switchType,
     pudding,
     bluetooth,
@@ -137,11 +223,19 @@ export default defineEventHandler(async (e) => {
     db.select().from(keycaps),
   ])
 
-  const possibleKeyboards = filterKeyboards(keyboardWithColorsOptions, { maxPrice, format, colors, bluetooth, wireless })
-  // rangiranje ce da ide po bojama, boje se daju sa tezinama, tako da donose 2 ili 1 poen.
-  const possibleSwitches = filterSwitches(switchOptions, switchType)
-  const possibleKeycaps = filterKeycaps(keycapOptions, { pudding, colors })
+  const keyboardResponse = filterKeyboards(keyboardWithColorsOptions, { maxPrice, format, mainColor, otherColor, bluetooth, wireless })
+  const switchResponse = filterSwitches(switchOptions, switchType)
+  const keycapResponse = filterKeycaps(keycapOptions, { pudding, mainColor, otherColor })
+  const prebuilts = filterPrebuiltKeyboards(keyboardWithColorsOptions, { maxPrice, format, mainColor, otherColor, bluetooth, wireless })
 
-  const possiblePrebuilts = filterPrebuiltKeyboards(keyboardWithColorsOptions, { maxPrice, format, colors, bluetooth, wireless })
+  return {
+    matchingKeyboards: keyboardResponse.matching,
+    otherKeyboards: keyboardResponse.other,
+    matchingSwitches: switchResponse.matching,
+    otherSwitches: switchResponse.other,
+    matchingKeycaps: keycapResponse.matching,
+    otherKeycaps: keycapResponse.other,
+    prebuilts,
+  }
 })
 
